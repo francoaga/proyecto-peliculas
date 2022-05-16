08f37a6adbe8704f90b2988f9eb12f61f10f5b19
Florencia Di Marco <flor_di_marco@hotmail.com>
2022-05-16 03:04:51 -0300

    cambios

--- js/login.js
+++ js/login.js
@@ -1,27 +1,63 @@
-function login(e) {
-  let administradores= [{email: "fandresgroselle@gmail.com", password: "12356"}];
-  event.preventDefault();
+const mailInput = document.getElementById("mail");
+const contraseñaInput = document.getElementById("contraseña");
+const result = document.getElementById('result');
 
-  let email = document.getElementById("email").value;
-  let psw = document.getElementById("psw").value;
-  let result = document.getElementById('result');
-
-  let user = localStorage.getItem(email);
-  let data = JSON.parse(user);
-  console.log(data);
-
-  if(user === null) {
-    result.innerHTML = "Ingrese un email o contraseña valida"
-  } else if(email === administradores[0].email && psw === administradores[0].password) {
-    window.location.href= "./peliculasConAdmin.html";
-  } else if (email === data.email && psw === data.psw) {
-    window.location.href= "./peliculas.html";
-  } else {
-    result.innerHTML = "Ingrese un email o contraseña valida"
-  }
+//Funcion que limpia el login
+function limpiarLogin() {
+  mailInput.value = '';
+  contraseñaInput.value = '';
 }
 
-function registrarse(e) {
-  event.preventDefault();
-   window.location.href="./signUp.html"
+//Funciones generales de local storage
+function traerLocalStorage(nombreLS) {
+  return JSON.parse(localStorage.getItem(nombreLS));
+}
+
+function guardarLS(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+
+//Funcion que chequea si el usuario es admin con su pass
+function checkIfAdmin(mail, contraseña) {
+  return (mail === 'admin@gmail.com' && contraseña === '1234');
+}
+
+function checkInputs(mail, contraseña) {
+  return (mail && contraseña);
+}
+
+function checkIfUserExists (mail, contraseña) {
+  let users = traerLocalStorage("usuarios") || [];
+  console.log('users', users)
+  return users.find(usuario => usuario.mail === mail && usuario.contraseña === contraseña);
+}
+
+//FUNCIONES DEL LOGIN
+//Login - Admin y usuario 
+function loginmail() {
+  let mail = mailInput.value;
+  let contraseña = contraseñaInput.value;
+  if (checkInputs(mail, contraseña)) {
+    if (checkIfAdmin(mail, contraseña)) {
+      guardarLS('mailLogueado', {mail, contraseña});
+      window.location.href = "./peliculas.html";
+    } else {
+      let user = checkIfUserExists(mail, contraseña);
+      console.log('user', user)
+      if (user) {
+        guardarLS('mailLogueado', user);
+        location.href = "peliculas.html";
+      } else {
+        result.innerHTML = "Usuario no existente";
+      }
+    }
+  } else {
+    result.innerHTML = "Email o contraseña incorrecto";
+  }
+  limpiarLogin();
+}
+
+function registrarse() {
+  window.location.href = "./signUp.html"
 }
--- js/signup.js
+++ js/signup.js
@@ -1,23 +1,64 @@
-function signup(e){
-  event.preventDefault();
+//Funcion generar ID para clases
+function generarId(length) {
+  let id = "";
+  let characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    id += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return id;
+}
 
-let email = document.getElementById("email").value;
-let psw = document.getElementById("psw").value
-
-let user = {
-  email: email,
-  psw: psw
-};
-
-let json = JSON.stringify(user);
-localStorage.setItem(email, json);
-alert("usuario creado")
-window.location.href="index.html";
-
+//Funcion que limpia el login
+function limpiarLogin() {
+  document.getElementById("mail").value = '';
+  document.getElementById("contraseña").value = '';
 }
 
 
-function volver(e) {
-  event.preventDefault();
-   window.location.href="../index.html"
+//Funciones generales de local storage
+function traerLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+function guardarLS(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+class usuario {
+  constructor (mail, contraseña) {
+    this.id = generarId(20)
+    this.mail = mail;
+    this.contraseña = contraseña;
+  }
+}
+
+//Funcion que agrega un nuevo usuario
+function crearUsuario() {
+  let mail = document.getElementById("mail").value;
+  let contraseña = document.getElementById("contraseña").value;
+
+  let usuarios = traerLocalStorage('usuarios');
+  if (!usuarios) {
+    usuarios = [];
+  }
+  let mailExiste = false;
+  if (usuarios.length > 0) {
+    mailExiste = usuarios.find(usuario => usuario.mail === mail);
+  }
+  if (mailExiste) {
+    result.innerHTML = "Este mail ya sido registrado"
+  } else if (mail === null || contraseña === null) {
+    return;
+  } else {
+    let nuevoUsuario = new usuario(mail, contraseña);
+    usuarios.push(nuevoUsuario);
+    guardarLS('usuarios', usuarios);
+    limpiarLogin();
+  }
+}
+
+function volver() {
+  window.location.href = "./index.html"
 }
